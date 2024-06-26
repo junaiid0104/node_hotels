c5a42b3f3f3ee26376a736f6079464d30c312b49
Junaid Mohd <junaiid0104@gmail.com>
2024-06-26 23:35:50 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
 
 
-const mongoURL = "mongodb://localhost:27017/hotels";
+ //const mongurl = process.env.MONGODB_URL_LOCAL;
+const mongoURL = process.env.MONGODB_URL;
 
 mongoose.connect(mongoURL, {
     useNewUrlParser:true,
-    useUnifiedTopology:true
+    useUnifiedTopology:true,
+    ssl: true,
+    serverSelectionTimeoutMS: 50000, // Increase timeout to 50 seconds
+    socketTimeoutMS: 45000,  
+   
 })
 
 const db = mongoose.connection;
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require ("express");
 const app = express();
 const db = require("./db");
+require('dotenv').config();
+
+
 
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
 
 
 app.get('/', (req,res) => {
@@ -21,7 +26,10 @@ app.use('/person', personRoutes);
 const menuRoutes = require("./routes/menuRoutes");
 app.use('/menu', menuRoutes);
 
-app.listen(3000, () => {
+
+
+
+app.listen(PORT, () => {
 
     console.log("server started!!");
 })
